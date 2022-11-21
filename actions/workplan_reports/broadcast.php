0b916b4cac60b3897169f631a567de597dd33440
hamzahfauzy <hamzahfauzy97@gmail.com>
2022-11-21 12:59:39 +0700

    update notif

--- actions/workplan_reports/broadcast.php
+++ actions/workplan_reports/broadcast.php
@@ -8,9 +8,12 @@ if(get_role(auth()->user->id)->name == 'administrator')
     $users = $db->all('users',['phone'=>['IS NOT','NULL']]);
     $workplan_report = $db->single('workplan_reports',['id'=>$_GET['id']]);
     
+    $message = "*Notifikasi Aplikasi Lintang - LPJ*
+".$workplan_report->title;
+    
     foreach($users as $user)
     {
-        Wa::sendMessage($user->phone, $workplan_report->title);
+        Wa::sendMessage($user->phone, $message);
     }
     
     set_flash_msg(['success'=>'LPJ berhasil di siarkan']);
--- actions/workplans/broadcast.php
+++ actions/workplans/broadcast.php
@@ -7,10 +7,13 @@ if(get_role(auth()->user->id)->name == 'administrator')
 {
     $users = $db->all('users',['phone'=>['IS NOT','NULL']]);
     $workplan = $db->single('workplans',['id'=>$_GET['id']]);
+
+    $message = "*Notifikasi Aplikasi Lintang - Program Kerja*
+".$workplan->content;
     
     foreach($users as $user)
     {
-        Wa::sendMessage($user->phone, $workplan->content);
+        Wa::sendMessage($user->phone, $message);
     }
     
     set_flash_msg(['success'=>'Program Kerja berhasil di siarkan']);
